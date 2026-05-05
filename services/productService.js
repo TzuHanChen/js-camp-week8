50f52ee7b3efb048b977b104ae304c5365271c5b
TzuHan_Chen <hahachentzuhan@gmail.com>
2026-05-05 18:11:01 +0800

    feat: 完成任務三

--- services/productService.js
+++ services/productService.js
@@ -13,6 +13,8 @@ async function getProducts() {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得產品陣列
   // 回傳格式：{ products, count: 產品數量 }
+  const products = await fetchProducts();
+  return { products, count: products.length };
 }
 
 /**
@@ -24,6 +26,10 @@ async function getProductsByCategory(category) {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，篩選出符合 category 的產品
   // 回傳格式：篩選後的產品陣列
+  const products = await fetchProducts();
+  return products.reduce((prev, cur) => {
+    return (cur.category === category) ? [...prev, cur] : prev;
+  }, []);
 }
 
 /**
@@ -35,6 +41,9 @@ async function getProductById(productId) {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，找出 id 符合的產品
   // 若找不到，回傳 null
+  const products = await fetchProducts();
+  const product = products.find(product => product.id === productId);
+  return product ? product : null;
 }
 
 /**
@@ -44,6 +53,8 @@ async function getProductById(productId) {
 async function getCategories() {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，代入到 utils getAllCategories()
+  const products = await fetchProducts();
+  return getAllCategories(products);
 }
 
 /**
@@ -63,6 +74,16 @@ function displayProducts(products) {
   //    原價：NT$ 1,000
   //    售價：NT$ 800 (8折)
   // ----------------------------------------
+  console.log('產品列表：');
+  console.log('----------------------------------------');
+  products.forEach((product, index) => {
+    console.log(`${index + 1}. ${product.title}`);
+    const spaces = ' '.repeat(index.toString().length) + '  ';
+    console.log(`${spaces}分類：${product.category}`);
+    console.log(`${spaces}原價：${formatCurrency(product.price)}`);
+    console.log(`${spaces}售價：${formatCurrency(product.origin_price)} (${getDiscountRate(product)})`);
+    console.log('----------------------------------------');
+  });
 }
 
 module.exports = {
